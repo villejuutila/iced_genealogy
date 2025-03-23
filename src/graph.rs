e700a5c93761adebee962a13fb5d9a5404d0165f
Ville Juutila <ville.juutila@gmail.com>
2025-03-23 05:32:46 +0200

    Initial commit

---
+++ src/graph.rs
@@ -0,0 +1,138 @@
+use iced::{
+    mouse,
+    widget::{
+        canvas::{self, Frame, Stroke},
+        Canvas,
+    },
+    Color, Element,
+    Length::Fill,
+    Point, Rectangle, Renderer, Size, Theme,
+};
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct GraphNode {
+    id: u128,
+    anchor: Point,
+    size: Size,
+}
+
+impl GraphNode {
+    pub fn new(anchor: Point) -> Self {
+        Self {
+            id: Uuid::new_v4().as_u128(),
+            anchor,
+            size: Size::new(100.0, 100.0),
+        }
+    }
+
+    pub fn is_inside(&self, point: Point) -> bool {
+        point.x >= self.anchor.x
+            && point.x <= self.anchor.x + self.size.width
+            && point.y >= self.anchor.y
+            && point.y <= self.anchor.y + self.size.height
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum GraphInteraction {
+    None,
+    HoverGraphNode(u128),
+}
+
+impl Default for GraphInteraction {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum GraphMessage {
+    InsertNode(GraphNode),
+}
+
+pub struct Graph {
+    nodes: Vec<GraphNode>,
+}
+
+impl Default for Graph {
+    fn default() -> Self {
+        Self { nodes: vec![] }
+    }
+}
+
+impl Graph {
+    pub fn view(&self) -> Element<GraphMessage> {
+        Canvas::new(self).width(Fill).height(Fill).into()
+    }
+
+    pub fn insert_node(&mut self, node: GraphNode) {
+        self.nodes.push(node);
+    }
+}
+
+impl canvas::Program<GraphMessage> for Graph {
+    type State = GraphInteraction;
+
+    fn update(
+        &self,
+        state: &mut Self::State,
+        event: canvas::Event,
+        _bounds: Rectangle,
+        cursor: mouse::Cursor,
+    ) -> (canvas::event::Status, Option<GraphMessage>) {
+        let mut message = None;
+        let Some(cursor_position) = cursor.position() else {
+            return (canvas::event::Status::Ignored, message);
+        };
+        for node in &self.nodes {
+            if node.is_inside(cursor_position) {
+                *state = GraphInteraction::HoverGraphNode(node.id);
+                return (canvas::event::Status::Captured, message);
+            }
+        }
+        match event {
+            canvas::Event::Mouse(mouse_event) => match mouse_event {
+                mouse::Event::ButtonReleased(button) => match button {
+                    mouse::Button::Left => message = Some(GraphMessage::InsertNode(GraphNode::new(cursor_position))),
+                    _ => *state = GraphInteraction::None,
+                },
+                _ => *state = GraphInteraction::None,
+            },
+            _ => *state = GraphInteraction::None,
+        }
+        (canvas::event::Status::Ignored, message)
+    }
+
+    fn draw(
+        &self,
+        interaction: &GraphInteraction,
+        renderer: &Renderer,
+        _theme: &Theme,
+        bounds: Rectangle,
+        _cursor: mouse::Cursor,
+    ) -> Vec<canvas::Geometry> {
+        let frames = self
+            .nodes
+            .iter()
+            .map(|node| {
+                let color = if interaction == &GraphInteraction::HoverGraphNode(node.id) {
+                    Color { a: 0.5, ..Color::WHITE }
+                } else {
+                    Color::WHITE
+                };
+                let mut frame = Frame::new(renderer, bounds.size());
+                frame.fill_rectangle(node.anchor, node.size, color);
+                frame.stroke_rectangle(
+                    node.anchor,
+                    node.size,
+                    Stroke::default()
+                        .with_width(2.0)
+                        .with_color(Color::from_rgb(255.0, 0.0, 0.0)),
+                );
+                frame.into_geometry()
+            })
+            .collect();
+        frames
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+mod graph;
+
+use graph::{Graph, GraphMessage};
+use iced::{
+    event,
+    widget::{column, container},
+    Element, Error, Event,
+    Length::Fill,
+    Subscription,
+};
+
+#[derive(Debug, Clone)]
+enum Message {
+    EventOccurred(Event),
+    Graph(GraphMessage),
+}
+
+struct App {
+    theme: iced::Theme,
+    graph: Graph,
+}
+
+impl App {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::EventOccurred(event) => self.on_event(event),
+            Message::Graph(graph_message) => match graph_message {
+                GraphMessage::InsertNode(node) => {
+                    self.graph.insert_node(node);
+                }
+            },
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let content = column![self.graph.view().map(Message::Graph)];
+
+        container(content).width(Fill).height(Fill).into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        event::listen().map(Message::EventOccurred)
+    }
+
+    fn on_event(&mut self, _event: Event) {}
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            theme: iced::Theme::default(),
+            graph: Graph::default(),
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    iced::application("Iced graph", App::update, App::view)
+        .subscription(App::subscription)
+        .antialiasing(true)
+        .run()
+}
